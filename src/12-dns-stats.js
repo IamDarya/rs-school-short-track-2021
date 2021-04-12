abf5882208425eb871986065d5632a107b006eff
IamDarya <daria.vyacheslavovna@gmail.com>
2021-04-12 20:24:00 +0200

    feat: implement solutions for task 12
    12.dns-stats

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,26 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const savedDomains = domains;
+  const obj = {};
+  for (let i = 0; i < savedDomains.length; i++) {
+    savedDomains[i] = savedDomains[i].split('.');
+    for (let j = 0; j < savedDomains[i].length; j++) {
+      let calc = j + 1;
+      while (calc < savedDomains[i].length) {
+        savedDomains[i][j] = savedDomains[i][calc].concat('.', savedDomains[i][j]);
+        calc++;
+      }
+      savedDomains[i][j] = '.'.concat(savedDomains[i][j]);
+      if (obj[savedDomains[i][j]] === undefined) {
+        obj[savedDomains[i][j]] = 1;
+      } else {
+        obj[savedDomains[i][j]]++;
+      }
+    }
+  }
+  return obj;
 }
 
 module.exports = getDNSStats;
